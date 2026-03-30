0f0f473a342678d3b930f808695d0a60cc3a37a7
MustafaEP <portakalm12@gmail.com>
2026-03-31 00:29:11 +0300

    feat: add authservice and userservice

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,64 @@
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from 'src/user/user.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private prisma: PrismaService,
+        private userService: UserService,
+    ) {}
+
+    /**
+     * Register a new user
+     */
+    async register(dto: RegisterDto) {
+        // Check if email is already in use
+        const existingUser = await this.userService.findByEmail(dto.email);
+        if (existingUser) {
+            throw new ConflictException('Email is already in use');
+        }
+
+        // Hash the password before saving the user
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(dto.password, salt);
+
+        // Create the user
+        const user = await this.userService.create({
+            email: dto.email,
+            name: dto.name,
+            passwordHash: passwordHash,
+        });
+        return {
+            id: user.id,
+            email: user.email,
+            name: user.name,   
+        };
+    }
+
+    /**
+     * Login a user
+     */
+    async login(dto: LoginDto) {
+        // Find the user by email
+        const user = await this.userService.findByEmail(dto.email);
+        if(!user || !user.passwordHash) {
+            throw new UnauthorizedException('Invalid email or password');
+        }
+
+        // Compare the provided password with the stored hash
+        const isPasswordValid = await bcrypt.compare(dto.password, user.passwordHash);
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Invalid email or password');
+        }
+
+        return {
+            id: user.id,
+            email: user.email,
+            name: user.name,
+        };
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // DTO'larda tanımlanmayan alanları otomatik olarak temizler
+    forbidNonWhitelisted: true, // Tanımlanmayan alanlar varsa hata fırlatır
+    transform: true, // Gelen verileri DTO sınıflarına dönüştürür
+  }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
